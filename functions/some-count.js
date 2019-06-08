5bd9cd9a7697788a63630df2359d66f03f9814ff
Benedicte Raae <raae@bgraphic.no>
2019-06-08 20:15:45 +0200

    Format prettier.

--- functions/some-count.js
+++ functions/some-count.js
@@ -1,12 +1,11 @@
-const cheerio = require('cheerio')
-const axios = require("axios")
-const addWeeks = require('date-fns/add_weeks')
-const format = require('date-fns/format')
-const Octokit = require('@octokit/rest')
+const cheerio = require("cheerio");
+const axios = require("axios");
+const addWeeks = require("date-fns/add_weeks");
+const format = require("date-fns/format");
+const Octokit = require("@octokit/rest");
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN
- })
-
+});
 
 const PARAMS = {
   owner: "raae",
@@ -20,62 +19,62 @@ const PARAMS = {
 
 const fileName = (weeksToAdd = 0) => {
   const date = addWeeks(new Date(), weeksToAdd);
-  return format(date, "YYYY-MM-DD") + ".json"
-}
+  return format(date, "YYYY-MM-DD") + ".json";
+};
 
 const fetchTwitterData = async () => {
-  const data = {}
-  try{
+  const data = {};
+  try {
     const page = await axios.get("https://twitter.com/raae");
     const $ = cheerio.load(page.data);
     const element$ = $(".ProfileNav-item--followers .ProfileNav-value");
-    data.count = parseInt(element$.attr('data-count'), 10);
-  } catch(error) {
-    data.error = error.message
+    data.count = parseInt(element$.attr("data-count"), 10);
+  } catch (error) {
+    data.error = error.message;
   }
 
   return data;
-}
+};
 
 const fetchInstagramData = async () => {
-  const data = {}
-  try{
+  const data = {};
+  try {
     const page = await axios.get("https://www.instagram.com/raae.codes/?__a=1");
     const json = page.data;
-    data.count = json.graphql.user.edge_followed_by.count
-  } catch(error) {
-    data.error = error.message
+    data.count = json.graphql.user.edge_followed_by.count;
+  } catch (error) {
+    data.error = error.message;
   }
 
   return data;
-}
+};
 
 const fetchThisWeeksData = async () => {
   const data = {
     twitter: await fetchTwitterData(),
     instagram: await fetchInstagramData()
-  }
+  };
 
   return data;
-}
+};
 
 const fetchLastWeeksData = async () => {
   try {
-    const {data} = await octokit.repos.getContents({
+    const { data } = await octokit.repos.getContents({
       ...PARAMS,
       path: fileName(-1)
     });
-    const content = Buffer.from(data.content, data.encoding).toString('utf8');
-    return JSON.parse(content)
+    const content = Buffer.from(data.content, data.encoding).toString("utf8");
+    return JSON.parse(content);
   } catch (error) {
     return {
       error: error.message
     };
   }
-}
+};
 
 const generateThisWeeksContent = (lastWeeksData, thisWeeksData) => {
-  if(lastWeeksData.error) return thisWeeksData;
+  if (lastWeeksData.error) return thisWeeksData;
 
   return {
     twitter: {
@@ -85,9 +84,9 @@ const generateThisWeeksContent = (lastWeeksData, thisWeeksData) => {
     instagram: {
       count: thisWeeksData.instagram.count,
       change: thisWeeksData.instagram.count - lastWeeksData.instagram.count
-    },
-  }
-}
+    }
+  };
+};
 
 exports.handler = async () => {
   const thisWeeksFileName = fileName();
@@ -99,22 +98,26 @@ exports.handler = async () => {
     ...PARAMS,
     path: thisWeeksFileName,
     message: "This weeks stats" + thisWeeksFileName + ".",
-    content: Buffer.from(JSON.stringify(content, null, 2)).toString('base64')
-  }
+    content: Buffer.from(JSON.stringify(content, null, 2)).toString("base64")
+  };
 
   try {
-    const file = await octokit.repos.getContents(params)
+    const file = await octokit.repos.getContents(params);
     params.sha = file.data.sha;
   } catch (error) {
     //Silence
   }
 
-  await octokit.repos.updateFile(params)
+  await octokit.repos.updateFile(params);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      content
-    }, null, 2)
-  }
+    body: JSON.stringify(
+      {
+        content
+      },
+      null,
+      2
+    )
+  };
 };
