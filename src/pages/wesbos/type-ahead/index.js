07f59e1865b44d13bcb92d847d0493e013c35f34
Benedicte Raae <raae@raae.codes>
2021-06-21 20:54:30 +0200

    Type ahead without highlighting

---
+++ src/pages/wesbos/type-ahead/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { Layout } from "../../../components/layout";
+import * as style from "./index.module.css";
+
+const ENDPOINT =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const findMatches = (cities, wordToMatch) => {
+  return cities.filter((place) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return place.city.match(regex) || place.state.match(regex);
+  });
+};
+
+const TypeAheadPage = () => {
+  const [cities, setCities] = useState([]);
+  const [wordToMatch, setWordToMatch] = useState("");
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(ENDPOINT);
+      const data = await response.json();
+      setCities(data);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    setMatches(findMatches(cities, wordToMatch));
+  }, [cities, wordToMatch]);
+
+  const handleInputChange = (event) => {
+    setWordToMatch(event.target.value);
+  };
+
+  return (
+    <Layout>
+      <form className={style.root}>
+        <input
+          type="text"
+          placeholder="City or State"
+          onChange={handleInputChange}
+          onKeyUp={handleInputChange}
+          value={wordToMatch}
+        />
+        <ul>
+          {!wordToMatch ? (
+            <>
+              <li>Filter for a city</li>
+              <li>or a state</li>
+            </>
+          ) : (
+            <>
+              {matches.map((place) => {
+                return (
+                  <li key={`${place.city}-${place.state}`}>
+                    <span>
+                      {place.city}, {place.state}
+                    </span>
+                    <span>{place.population}</span>
+                  </li>
+                );
+              })}
+            </>
+          )}
+        </ul>
+      </form>
+    </Layout>
+  );
+};
+
+export default TypeAheadPage;
