54c7eecab225f0bb97d46d4563179594ab61ba7b
Benedicte Raae <raae@raae.codes>
2021-06-21 21:21:59 +0200

    Copy and move root style to section

--- src/pages/wesbos/type-ahead/index.js
+++ src/pages/wesbos/type-ahead/index.js
@@ -38,39 +38,56 @@ const TypeAheadPage = () => {
 
   return (
     <Layout>
-      <form className={style.root}>
-        <input
-          type="text"
-          placeholder="City or State"
-          onChange={handleInputChange}
-          onKeyUp={handleInputChange}
-          value={wordToMatch}
-        />
-        <ul>
-          {!wordToMatch ? (
-            <>
-              <li>Filter for a city</li>
-              <li>or a state</li>
-            </>
-          ) : (
-            <>
-              {matches.map((place) => {
-                const regex = new RegExp(`(${wordToMatch})`, "ig");
-                const cityName = place.city.replace(regex, `<em>$1</em>`);
-                const placeName = place.state.replace(regex, `<em>$1</em>`);
-                return (
-                  <li
-                    key={`${place.city}-${place.state}`}
-                    dangerouslySetInnerHTML={{
-                      __html: `<span>${cityName}, ${placeName}</span><span>${place.population}</span>`,
-                    }}
-                  ></li>
-                );
-              })}
-            </>
-          )}
-        </ul>
-      </form>
+      <section className={style.root}>
+        <h1>Gatsbyfied Type Ahead</h1>
+        <p>
+          The WesBos Vanilla{" "}
+          <a href="https://javascript30.com/">Javascript30</a> Day 6 Challenge,
+          but done in React on Gatsby.
+        </p>
+        <p>
+          If you wanna see how I solved it check out{" "}
+          <a href="https://github.com/raae/raae.codes/pull/13">
+            the Pull Request
+          </a>
+          , feel free to leave a comment if you have a question or any type of
+          feedback.
+        </p>
+
+        <form>
+          <input
+            type="text"
+            placeholder="City or State"
+            onChange={handleInputChange}
+            onKeyUp={handleInputChange}
+            value={wordToMatch}
+          />
+          <ul>
+            {!wordToMatch ? (
+              <>
+                <li>Filter for a city</li>
+                <li>or a state</li>
+              </>
+            ) : (
+              <>
+                {matches.map((place) => {
+                  const regex = new RegExp(`(${wordToMatch})`, "ig");
+                  const cityName = place.city.replace(regex, `<em>$1</em>`);
+                  const placeName = place.state.replace(regex, `<em>$1</em>`);
+                  return (
+                    <li
+                      key={`${place.city}-${place.state}`}
+                      dangerouslySetInnerHTML={{
+                        __html: `<span>${cityName}, ${placeName}</span><span>${place.population}</span>`,
+                      }}
+                    ></li>
+                  );
+                })}
+              </>
+            )}
+          </ul>
+        </form>
+      </section>
     </Layout>
   );
 };
