eefe17d6b94d22f0c87a2a395d9ad900da1139aa
Benedicte Raae <raae@raae.codes>
2021-06-21 21:35:28 +0200

    Add copy and links

--- src/pages/wesbos/css-variables/index.js
+++ src/pages/wesbos/css-variables/index.js
@@ -3,7 +3,7 @@ import { useCallback } from "react";
 import { useState } from "react";
 import * as style from "./index.module.css";
 
-import Layout from "../../../components/layout";
+import { Layout } from "../../../components/layout";
 
 const SIZING = {
   spacing: "px",
@@ -48,8 +48,22 @@ const CssVariablesPage = ({ location }) => {
   };
 
   return (
-    <Layout title="WesBos Javascript30 / 03 CSS Variables" location={location}>
+    <Layout>
       <section className={style.root} ref={rootRef}>
+        <h1>CSS Variables</h1>
+        <p>
+          The WesBos Vanilla{" "}
+          <a href="https://javascript30.com/">Javascript30</a> Day 3 Challenge,
+          but done in React on Gatsby.
+        </p>
+        <p>
+          If you wanna see how I solved it check out{" "}
+          <a href="https://github.com/raae/raae.codes/pull/11">
+            the Pull Request
+          </a>
+          , feel free to leave a comment if you have a question or any type of
+          feedback.
+        </p>
         <h2>
           Update CSS Variables with <span className={style.hl}>JS</span>
         </h2>
@@ -87,7 +101,10 @@ const CssVariablesPage = ({ location }) => {
             onChange={handleChange("color")}
           />
         </div>
-        <img src="https://source.unsplash.com/7bwQXzbF6KE/800x500"></img>
+        <img
+          alt="Random nature shot"
+          src="https://source.unsplash.com/7bwQXzbF6KE/800x500"
+        ></img>
       </section>
     </Layout>
   );
