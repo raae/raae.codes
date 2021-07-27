10f366ff970c12b66bb93289864432669b0c0b45
Benedicte Raae <raae@raae.codes>
2021-07-27 13:23:01 +0200

    wip: error handling

--- src/api/gsf-week2-vote.js
+++ src/api/gsf-week2-vote.js
@@ -1,10 +1,14 @@
 const crypto = require("crypto");
 
 export default async function corsHandler(req, res) {
-  const ipHashWithSalt = crypto
-    .createHash("sha256")
-    .update(req.ip + process.env.ipSalt)
-    .digest("hex");
+  try {
+    const ipHashWithSalt = crypto
+      .createHash("sha256")
+      .update(req.ip + process.env.IP_HASH_SALT)
+      .digest("hex");
 
-  res.json({ ip: req.ip, hash: ipHashWithSalt });
+    res.json({ ip: req.ip, hash: ipHashWithSalt });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
